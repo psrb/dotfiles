8fce415d5b91b8a4ad63798f33f54f2b0722cb11
Pascal Urban <me@psrb.io>
2014-11-30 20:13:51 +0100

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 printf "Cloning oh-my-zsh \t"
-if [[ ! -d .oh-my-zsh ]]; then
+if [ ! -d .oh-my-zsh ]; then
     printf "\n"
     git clone git://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh
     printf "\n"
@@ -10,7 +10,7 @@ else
 fi
 
 printf "Cloning Vundle \t"
-if [[ ! -d .vim/bundle/Vundle.vim ]]; then
+if [ ! -d .vim/bundle/Vundle.vim ]; then
     printf "\n"
     git clone https://github.com/gmarik/Vundle.vim.git .vim/bundle/Vundle.vim
     printf "\n"
@@ -21,12 +21,12 @@ fi
 
 printf "Creating symlinks\n"
 DIR=$(cd $(dirname $0); pwd)
-[ ! -e ~/.tmux.conf ] && ln -s $DIR/.tmux.conf ~/.tmux.conf
-[ ! -d ~/.vim ] && ln -s $DIR/.vim ~/.vim
-[ ! -e ~/.vimrc ] && ln -s $DIR/.vimrc ~/.vimrc
-[ ! -d ~/.oh-my-zsh ] && ln -s $DIR/.oh-my-zsh ~/.oh-my-zsh
-[ ! -e ~/.zshenv ] && ln -s $DIR/.zshenv ~/.zshenv
-[ ! -e ~/.zshrc ] && ln -s $DIR/.zshrc ~/.zshrc
+[ ! -h ~/.tmux.conf ] && ln -s $DIR/.tmux.conf ~/.tmux.conf
+[ ! -h ~/.vim ] && ln -s $DIR/.vim ~/.vim
+[ ! -h ~/.vimrc ] && ln -s $DIR/.vimrc ~/.vimrc
+[ ! -h ~/.oh-my-zsh ] && ln -s $DIR/.oh-my-zsh ~/.oh-my-zsh
+[ ! -h ~/.zshenv ] && ln -s $DIR/.zshenv ~/.zshenv
+[ ! -h ~/.zshrc ] && ln -s $DIR/.zshrc ~/.zshrc
 
 printf "Creating directories\n"
 mkdir -p ~/.vimbackup
