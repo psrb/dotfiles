e4f189c7772c634f243d0008417b5e685f2f9a5f
Pascal Urban <me@psrb.io>
2014-12-15 20:22:22 +0100

    Updated install.sh and README

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 dotfiles
 ========
+A collection of my various dotfiles.
 
 ## Usage
-    ./install.sh
+`./install.sh` downloads oh-my-zsh and vundle, creates all necessary symlinks and folders and installs the plugins for Vim.
 
+If the environment variable `VIM_LIGHT_INSTALL` is set some Vim plugins will not be installed.
 
+## Dependencies
+ - YouCompleteMe: clang, needs to be compiled
+ - Clang-Format: clang-format
+ - Tagbar: CTags >= 5.5
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-printf "Cloning oh-my-zsh \t"
+printf "Cloning oh-my-zsh ...\t"
 if [ ! -d .oh-my-zsh ]; then
     printf "\n"
     git clone git://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh
@@ -9,14 +9,17 @@ else
     printf "already installed!\n"
 fi
 
-printf "Cloning Vundle \t"
+printf "Cloning Vundle    ...\t"
 if [ ! -d .vim/bundle/Vundle.vim ]; then
     printf "\n"
     git clone https://github.com/gmarik/Vundle.vim.git .vim/bundle/Vundle.vim
     printf "\n"
 else
-    printf "alread installed!\n"
+    printf "already installed!\n"
 fi
+
+printf "Creating folders\n"
+[ ! -d ~/.vimundo ] && mkdir ~/.vimundo
 
 printf "Creating symlinks\n"
 DIR=$(cd $(dirname $0); pwd)
@@ -26,6 +29,16 @@ DIR=$(cd $(dirname $0); pwd)
 [ ! -h ~/.oh-my-zsh ] && ln -s $DIR/.oh-my-zsh ~/.oh-my-zsh
 [ ! -h ~/.zshenv ] && ln -s $DIR/.zshenv ~/.zshenv
 [ ! -h ~/.zshrc ] && ln -s $DIR/.zshrc ~/.zshrc
+
+
+printf "Installing Vim plugins\n"
+
+read -p "Light install (less plugins)? [yN]:" yn
+if [ "$yn" = "y" ] || [ "$yn" = "Y" ]; then
+    echo "Remeber to export the environment variable 'VIM_LIGHT_INSTALL'!"
+    export VIM_LIGHT_INSTALL=1
+fi
+vim +PluginInstall
 
 printf "Finished\n"
 
