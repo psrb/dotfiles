89982c21b6ebff607e8011d5b1ea6cd04ec0bf39
Pascal Urban <me@psrb.io>
2015-01-16 12:59:55 +0100

    Removed the dot

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 printf "Cloning oh-my-zsh ...\t"
-if [ ! -d .oh-my-zsh ]; then
+if [ ! -d oh-my-zsh ]; then
     printf "\n"
-    git clone git://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh
+    git clone git://github.com/robbyrussell/oh-my-zsh.git oh-my-zsh
     printf "\n"
 else
     printf "already installed!\n"
 fi
 
 printf "Cloning Vundle    ...\t"
-if [ ! -d .vim/bundle/Vundle.vim ]; then
+if [ ! -d vim/bundle/Vundle.vim ]; then
     printf "\n"
-    git clone https://github.com/gmarik/Vundle.vim.git .vim/bundle/Vundle.vim
+    git clone https://github.com/gmarik/Vundle.vim.git vim/bundle/Vundle.vim
     printf "\n"
 else
     printf "already installed!\n"
@@ -23,12 +23,12 @@ printf "Creating folders\n"
 
 printf "Creating symlinks\n"
 DIR=$(cd $(dirname $0); pwd)
-[ ! -h ~/.tmux.conf ] && ln -s $DIR/.tmux.conf ~/.tmux.conf
-[ ! -h ~/.vim ] && ln -s $DIR/.vim ~/.vim
-[ ! -h ~/.vimrc ] && ln -s $DIR/.vimrc ~/.vimrc
-[ ! -h ~/.oh-my-zsh ] && ln -s $DIR/.oh-my-zsh ~/.oh-my-zsh
-[ ! -h ~/.zshenv ] && ln -s $DIR/.zshenv ~/.zshenv
-[ ! -h ~/.zshrc ] && ln -s $DIR/.zshrc ~/.zshrc
+[ ! -h ~/.tmux.conf ] && ln -s $DIR/tmux.conf ~/.tmux.conf
+[ ! -h ~/.vim ] && ln -s $DIR/vim ~/.vim
+[ ! -h ~/.vimrc ] && ln -s $DIR/vimrc ~/.vimrc
+[ ! -h ~/.oh-my-zsh ] && ln -s $DIR/oh-my-zsh ~/.oh-my-zsh
+[ ! -h ~/.zshenv ] && ln -s $DIR/zshenv ~/.zshenv
+[ ! -h ~/.zshrc ] && ln -s $DIR/zshrc ~/.zshrc
 
 
 printf "Installing Vim plugins\n"
