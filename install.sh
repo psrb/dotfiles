48da36025aa0b1748eec327536ab722b9a901258
Pascal Urban <me@psrb.io>
2015-03-25 17:01:20 +0100

    Check if vim is installed, other fixes

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+command -v vim >/dev/null 2>&1
+VIM_INSTALLED=$?
+if [ $VIM_INSTALLED -ne 0 ]; then
+    printf "Vim is not installed.\n"
+    exit 1
+fi
+
 printf "Cloning oh-my-zsh ...\t"
 if [ ! -d oh-my-zsh ]; then
     printf "\n"
@@ -12,7 +19,7 @@ fi
 printf "Cloning plug.vim    ...\t"
 if [ ! -f vim/autoload/plug.vim ]; then
     printf "\n"
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    curl -fLo vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     printf "\n"
 else
@@ -23,14 +30,13 @@ printf "Creating folders\n"
 [ ! -d ~/.vimundo ] && mkdir ~/.vimundo
 
 printf "Creating symlinks\n"
-DIR=$(cd $(dirname $0); pwd)
-[ ! -h ~/.tmux.conf ] && ln -s $DIR/tmux.conf ~/.tmux.conf
-[ ! -h ~/.vim ] && ln -s $DIR/vim ~/.vim
-[ ! -h ~/.vimrc ] && ln -s $DIR/vimrc ~/.vimrc
-[ ! -h ~/.oh-my-zsh ] && ln -s $DIR/oh-my-zsh ~/.oh-my-zsh
-[ ! -h ~/.zshenv ] && ln -s $DIR/zshenv ~/.zshenv
-[ ! -h ~/.zshrc ] && ln -s $DIR/zshrc ~/.zshrc
-
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+[ ! -h ~/.tmux.conf ] && ln -s $SCRIPT_DIR/tmux.conf ~/.tmux.conf
+[ ! -h ~/.vim ] && ln -s $SCRIPT_DIR/vim ~/.vim
+[ ! -h ~/.vimrc ] && ln -s $SCRIPT_DIR/vimrc ~/.vimrc
+[ ! -h ~/.oh-my-zsh ] && ln -s $SCRIPT_DIR/oh-my-zsh ~/.oh-my-zsh
+[ ! -h ~/.zshenv ] && ln -s $SCRIPT_DIR/zshenv ~/.zshenv
+[ ! -h ~/.zshrc ] && ln -s $SCRIPT_DIR/zshrc ~/.zshrc
 
 printf "Installing Vim plugins\n"
 
