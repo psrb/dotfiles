8e4da3b2b269439da2a228890d7a7581b2e718dc
Pascal Urban <me@psrb.io>
2016-03-06 12:47:31 +0100

    Fixed lint warnings for install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 ask() {
-    read -p "$1 [yN]: " yn
+    printf "%s [yN]: " "$1"
+    read -r yn
     if [ "$yn" = "y" ] || [ "$yn" = "Y" ]; then
         return 0
     fi
@@ -9,8 +10,8 @@ ask() {
 }
 
 check_installed() {
-    printf " %s: " $1
-    if command -v $1 >/dev/null 2>&1; then
+    printf " %s: " "$1"
+    if command -v "$1" >/dev/null 2>&1; then
         echo "installed!"
     else
         echo "not installed!"
@@ -25,35 +26,35 @@ create_link() {
     file=$1
     link_name=$2
 
-    printf " Link: %s -> %s\n" $link_name $file
+    printf " Link: %s -> %s\n" "$link_name" "$file"
 
-    if [ -h $link_name ]; then # is a symbolic link
-        rm $link_name
-    elif [ -e $link_name ]; then # other file exists
+    if [ -h "$link_name" ]; then # is a symbolic link
+        rm "$link_name"
+    elif [ -e "$link_name" ]; then # other file exists
         if ask " File \"$link_name\" exists! Do you want to overwrite it? "; then
-            if [ -d $link_name ]; then
-                rm -r $link_name
+            if [ -d "$link_name" ]; then
+                rm -r "$link_name"
             else
-                rm $link_name
+                rm "$link_name"
             fi
         else
             return
         fi
     fi
 
-    ln -s $file $link_name
+    ln -s "$file" "$link_name"
 }
 
 download_plug_vim() {
     file_path=$1
 
     printf "Downloading plug.vim ...\t"
-    if [ -e $file_path ]; then
+    if [ -e "$file_path" ]; then
         echo "already downloaded!"
         return
     fi
 
-    if curl -sfLo $file_path --create-dirs \
+    if curl -sfLo "$file_path" --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
     then
         echo "downloaded!"
@@ -70,17 +71,17 @@ check_installed vim
 check_installed zsh
 echo
 
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
+SCRIPT_DIR=$(cd "$(dirname "$0")" || exit 1; pwd)
 
 echo "Creating links"
-create_link $SCRIPT_DIR/latexmkrc ~/.latexmkrc
-create_link $SCRIPT_DIR/tmux.conf ~/.tmux.conf
-create_link $SCRIPT_DIR/vim ~/.vim
-create_link $SCRIPT_DIR/vim/vimrc ~/.vimrc
-create_link $SCRIPT_DIR/zsh ~/.zsh
-create_link $SCRIPT_DIR/zsh/zprofile ~/.zprofile
-create_link $SCRIPT_DIR/zsh/zshenv ~/.zshenv
-create_link $SCRIPT_DIR/zsh/zshrc ~/.zshrc
+create_link "$SCRIPT_DIR/latexmkrc" ~/.latexmkrc
+create_link "$SCRIPT_DIR/tmux.conf" ~/.tmux.conf
+create_link "$SCRIPT_DIR/vim" ~/.vim
+create_link "$SCRIPT_DIR/vim/vimrc" ~/.vimrc
+create_link "$SCRIPT_DIR/zsh" ~/.zsh
+create_link "$SCRIPT_DIR/zsh/zprofile" ~/.zprofile
+create_link "$SCRIPT_DIR/zsh/zshenv" ~/.zshenv
+create_link "$SCRIPT_DIR/zsh/zshrc" ~/.zshrc
 echo
 
 echo "Creating folders"
@@ -88,7 +89,7 @@ echo " Folder: ~/.vimundo"
 [ ! -d ~/.vimundo ] && mkdir ~/.vimundo
 echo
 
-download_plug_vim $SCRIPT_DIR/vim/autoload/plug.vim
+download_plug_vim "$SCRIPT_DIR/vim/autoload/plug.vim"
 echo
 
 printf "Installing Vim plugins\n"
