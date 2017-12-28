d3d667d1a3afd009c4802400ae71df90bce66f29
Pascal Urban <me@psrb.io>
2017-12-28 21:13:36 +0100

    ZSH: Fix colors of man/less

--- zsh/modules/functions.zsh
+++ zsh/modules/functions.zsh
@@ -4,7 +4,7 @@ function ccd() {
 }
 
 # Colored man pages
-# Source: https://linuxtidbits.wordpress.com/2009/03/23/less-colors-for-man-pages/
+# Source: https://wiki.archlinux.org/index.php/Color_output_in_console#man
 # LESS_TERMCAP_mb: begin blinking
 # LESS_TERMCAP_md: begin bold
 # LESS_TERMCAP_me: end mode
@@ -12,15 +12,15 @@ function ccd() {
 # LESS_TERMCAP_so: begin standout-mode - info box
 # LESS_TERMCAP_ue: end underline
 # LESS_TERMCAP_us: begin underline
+# 8-bit color codes: https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit
 function man() {
-    env \
-    LESS_TERMCAP_mb=$'\e[1;31m' \
-    LESS_TERMCAP_md=$'\e[1;31m' \
+    LESS_TERMCAP_mb=$'\e[1;38;5;1m' \
+    LESS_TERMCAP_md=$'\e[1;38;5;1m' \
     LESS_TERMCAP_me=$'\e[0m' \
     LESS_TERMCAP_se=$'\e[0m' \
-    LESS_TERMCAP_so=$'\e[1;44;33m' \
+    LESS_TERMCAP_so=$'\e[1;38;5;2m' \
     LESS_TERMCAP_ue=$'\e[0m' \
-    LESS_TERMCAP_us=$'\e[1;32m' \
+    LESS_TERMCAP_us=$'\e[1;38;5;2m' \
     PAGER=/usr/bin/less \
-    man "$@"
+    command man "$@"
 }
