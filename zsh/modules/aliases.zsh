d9339d1231454616a6ebed203dd635927b72bdf6
Pascal Urban <mail@psrb.io>
2018-04-02 18:57:59 +0200

    Move git aliases into git config

--- zsh/modules/aliases.zsh
+++ zsh/modules/aliases.zsh
@@ -6,17 +6,6 @@ alias l="ls -alh"
 alias grep="grep --color=auto"
 alias history-all="fc -E -l 1"
 
-# Git
-alias gst="git status"
-alias ga="git add"
-alias gf="git fetch"
-alias gp="git pull"
-alias gco="git checkout"
-alias gc="git commit"
-alias gd="git diff"
-alias gdt="git difftool"
-alias gll="git log --pretty=\"%C(auto)%h%d %Cred%s%Creset by %an (%cr)\"" # one line: Hash (ref names) Subject by Author (relative date)
-
 # Tmux
 alias tmuxa="tmux attach"
 alias tmuxls="tmux list-sessions"
