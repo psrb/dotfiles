4bce9f6e717b244bde8cad8afe10b608bffb0212
Pascal Urban <mail@psrb.io>
2018-04-02 18:20:13 +0200

    Add git config

--- install.sh
+++ install.sh
@@ -90,6 +90,8 @@ create_link "$SCRIPT_DIR/zsh" ~/.zsh
 create_link "$SCRIPT_DIR/zsh/zprofile" ~/.zprofile
 create_link "$SCRIPT_DIR/zsh/zshenv" ~/.zshenv
 create_link "$SCRIPT_DIR/zsh/zshrc" ~/.zshrc
+create_link "$SCRIPT_DIR/.gitconfig" ~/.gitconfig
+create_link "$SCRIPT_DIR/.gitignore_global" ~/.gitignore_global
 
 if [ "$(uname)" = "Darwin" ]
 then
